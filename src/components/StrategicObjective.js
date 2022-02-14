cdd64cb9a4eba0e89fa63f9b46c8985f50e9378e
unknown <chris.mooney@blackflagbarbell.com.au>
2022-02-14 15:54:49 +1100

    Limited objective component renders to three, error message/alert

--- src/components/StrategicObjective.js
+++ src/components/StrategicObjective.js
@@ -3,30 +3,39 @@ import ObjectiveComponent from './ObjectiveComponent.js'
 
 function StrategicObjective() {
 
+  //Creating default array to show on page load
+  let objectives = [ <ObjectiveComponent /> ];
+  //Using state to maintain/store objectives
+  const [ arr, setArr ] = useState(objectives);
   //Using state as counter for how many objectives to display
   const [ state, setState ] = useState(1);
+
   //Updating counter/state with each 'Add Objective' button click
-  function addObjective() {
+  const addObjective = e => {
+    e.preventDefault();
+
     setState(state+1);
-    console.log(state);
-  }
 
-  function displayObjectives() {
-    let objectives = [];
+    let newObj = <ObjectiveComponent />;
 
-    for (let i=0; i < state; i++) {
-      objectives.push(
-        <div key={i}>
-          <ObjectiveComponent />
-        </div>
-      )
+    //Again, not sure why it isn't counting the first rendered component. Fix if time.
+    if (arr.length <=2) {
+      setArr(arr => [...arr,
+      newObj
+      ]);
+    } else {
+      alert("Cannot add more than three strategies.")
     }
-    return objectives;
   }
+
 
   return(
     <div>
-      {displayObjectives()}
+      {arr.map(item => {
+        return (
+            <ObjectiveComponent />
+        );
+      })}
       <button className="add-objective-button" onClick={addObjective}>+<a> Add Objective</a></button>
     </div>
   );
